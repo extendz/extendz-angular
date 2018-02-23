32c255b0726331cdf63cc49de2e181b0eb3325eb
hrandika <hrandika@hotmail.com>
2018-02-23 10:23:05 +0530

    doc updates

--- src/app/modules/api/api-root-example/api-root-example.module.ts
+++ src/app/modules/api/api-root-example/api-root-example.module.ts
@@ -3,12 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ApiRootExampleRoutingModule } from './api-root-example-routing.module';
 import { ApiRootExampleComponent } from './api-root-example.component';
+import { MatTabsModule } from '@angular/material';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    ApiRootExampleRoutingModule
-  ],
+  imports: [CommonModule, ApiRootExampleRoutingModule, MatTabsModule],
   declarations: [ApiRootExampleComponent]
 })
 export class ApiRootExampleModule {}
