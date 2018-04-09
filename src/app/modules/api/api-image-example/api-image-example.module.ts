5b6fe948be2e9c2199302078a6e307a0693a84f9
Randika <hrandika@hotmail.com>
2018-04-09 22:55:38 +0530

    position adedd

--- src/app/modules/api/api-image-example/api-image-example.module.ts
+++ src/app/modules/api/api-image-example/api-image-example.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material';
+
 import { ApiImageExampleComponent } from './api-image-example.component';
 import { ApiImageExampleRoutingModule } from './api-image-example-routing.module';
-import { MatTabsModule } from '@angular/material';
-import { ExtendzDirectiveModule } from '../../../../platform/common/directives/extendz-directives.module';
-import { ExtendzRestModule } from '../../../../platform/common/services/rest/extendz-rest.module';
+
+import { ExtendzDirectiveModule, ExtendzRestModule } from '../../../../platform';
 
 @NgModule({
   imports: [
--- src/platform/common/directives/api-image.directive.ts
+++ src/platform/common/directives/api-image.directive.ts
@@ -1,22 +1,28 @@
 import { Directive, ElementRef, Input, OnInit, Renderer } from '@angular/core';
-import { ObjectWithLinks } from '../services';
 import { HttpClient } from '@angular/common/http';
+
 import { Subscription } from 'rxjs/Subscription';
 import { finalize } from 'rxjs/operators/finalize';
+
+import { ObjectWithLinks } from '../services';
 
 @Directive({
   selector: '[extApiImage]'
 })
 export class ApiImageDirective implements OnInit {
-  /**
-   * URL for the image.
-   */
+  /** URL for the image   */
   @Input() url: string;
 
+  /** In case of mutiple images. Whitch image to to shown */
+  @Input() position: number = 0;
+
+  /** Object to extract the image */
   @Input() item: ObjectWithLinks;
 
+  /** Property name for the image */
   @Input() property: string;
 
+  /** Defaut image */
   private defaultImage: string = `data:image/svg+xml,%3csvg fill='%23999999' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M0 0h24v24H0zm0 0h24v24H0zm21 19c0 1.1-.9 2-2 2H5c-1.1 0-2-.9-2-2V5c0-1.1.9-2 2-2h14c1.1 0 2 .9 2 2' fill='none'/%3e%3cpath d='M0 0h24v24H0z' fill='none'/%3e%3cpath d='M21 5v6.59l-3-3.01-4 4.01-4-4-4 4-3-3.01V5c0-1.1.9-2 2-2h14c1.1 0 2 .9 2 2zm-3 6.42l3 3.01V19c0 1.1-.9 2-2 2H5c-1.1 0-2-.9-2-2v-6.58l3 2.99 4-4 4 4 4-3.99z'/%3e%3c/svg%3e`;
 
   constructor(private el: ElementRef, private renderer: Renderer, private http: HttpClient) {}
@@ -29,14 +35,14 @@ export class ApiImageDirective implements OnInit {
         this.url = this.getFileUrl(imageNames);
       } else {
         let list: string[] = <string[]>imageNames;
-        this.url = this.getFileUrl(list[0]);
+        this.url = this.getFileUrl(list[this.position]);
       }
       let sub: Subscription = this.http
         .head(this.url)
         .pipe(finalize(() => this.updateElemnet()))
         .subscribe(() => sub.unsubscribe(), error => (this.url = this.defaultImage));
     } else {
-      this.url = this.defaultImage;
+      if (!this.url) this.url = this.defaultImage;
       this.updateElemnet();
     }
   } // ngOnInit()
@@ -47,7 +53,7 @@ export class ApiImageDirective implements OnInit {
     } else {
       this.el.nativeElement.style.backgroundImage = 'url(' + this.url + ')';
     }
-  }
+  } // updateElemnet()
 
   private getUrl(): string {
     return this.item._links.self.href + '/' + this.property;
